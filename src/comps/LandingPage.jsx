94de6ada8fd5c079d6427f72894d2ff7f18e4158
adityasriv2317 <adityaxia9237@gmail.com>
2024-11-24 15:43:00 +0530

    responsive fix

--- src/comps/LandingPage.jsx
+++ src/comps/LandingPage.jsx
@@ -20,7 +20,7 @@ const LandingPage = () => {
 
   return (
     // landing page full bg
-    <div className="h-screen w-screen  bg-gradient-to-t from-violet-500 to-fuchsia-500 flex items-center justify-center bg-gray-100">
+    <div className="h-screen w-screen bg-gradient-to-t from-violet-500 to-fuchsia-500 flex items-center justify-center bg-gray-100">
       <Link to="/admin" className="absolute bg-[rgba(0,0,0,0.55)] rounded-xl shadow-md p-[10px] font-bold text-white top-3 right-5">
         <div className="">
           ADMIN
@@ -28,17 +28,17 @@ const LandingPage = () => {
       </Link>
 
       {/* panel */}
-      <div className="flex w-4/5 max-w-4xl px-[2em] bg-[rgba(0,0,0,0.4)] rounded-xl shadow-md">
+      <div className="flex flex-col md:flex-row w-4/5 max-w-4xl px-[2em] bg-[rgba(0,0,0,0.4)] rounded-xl shadow-md">
         {/* left */}
-        <div className="w-1/2 p-6 rounded-l-lg flex flex-col justify-center">
-          <h1 className="text-4xl font-bold text-white">QuizSutra</h1>
-          <p className="text-white mt-2">
+        <div className="w-full md:w-1/2 p-6 rounded-t-lg md:rounded-l-lg md:rounded-tr-none flex flex-col justify-center">
+          <h1 className="text-4xl font-bold text-white text-center md:text-left">QuizSutra</h1>
+          <p className="text-white mt-2 text-center md:text-left">
             powered by Cloud Computing Cell, AKGEC
           </p>
         </div>
 
         {/* right */}
-        <form className="w-1/2 p-8 text-white" onSubmit={joinRoom}>
+        <form className="w-full md:w-1/2 p-8 text-white" onSubmit={joinRoom}>
           <h2 className="text-2xl text-center font-bold mb-4">
             Join a Quiz Room
           </h2>
@@ -48,7 +48,6 @@ const LandingPage = () => {
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              //   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
               className="mt-1 block w-full border rounded-lg text-gray-600 border-gray-300 p-2 focus:outline"
               placeholder="Enter your username"
             />
--- src/comps/admin/AdminRoutes.jsx
+++ src/comps/admin/AdminRoutes.jsx
@@ -1,28 +1,28 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const AdminRoutes = () => {
-
   useEffect(() => {
     document.title = "Admin - QuizSutra";
-  }, []); 
+  }, []);
 
   return (
-    <div className="flex-col justify-center items-center bg-gradient-to-t from-violet-500 to-fuchsia-500 w-screen h-screen">
-      {/* title */}
-      <h1 className="text-[3em] text-center font-bold text-white [text-shadow:0_0_10px_purple]">
+    <div className="flex flex-col justify-start items-center bg-gradient-to-t from-violet-500 to-fuchsia-500 w-screen h-screen">
+      {/* Title */}
+      <h1 className="mt-1 text-[2em] md:text-[3em] text-center font-bold text-white [text-shadow:0_0_10px_purple]">
         ADMIN ROUTES
       </h1>
 
-      <Link to="/">
-        <div className="absolute p-[10px] font-bold text-white top-[12px] right-[10px] bg-[rgba(0,0,0,0.55)] rounded-xl shadow-md">
+      {/* Home Button */}
+      <Link to="/" className="absolute top-3 right-3">
+        <div className="p-2 md:p-[10px] font-bold text-white bg-[rgba(0,0,0,0.55)] rounded-xl shadow-md">
           HOME
         </div>
       </Link>
 
-      {/* panel */}
-      <div className="flex items-center mx-auto py-[2vh] justify-center mt-[20vh] w-[80%] max-w-4xl bg-[rgba(255,255,255,0.3)] rounded-xl shadow-md">
-        <ul className="text-[2em] items-center text-center">
+      {/* Panel */}
+      <div className="flex flex-col items-center mx-auto py-4 px-4 justify-center mt-44 md:mt-[20vh] w-[70%] md:w-[70%] max-w-4xl bg-[rgba(255,255,255,0.3)] rounded-xl shadow-md">
+        <ul className="text-[1.5em] md:text-[2em] text-center space-y-6">
           <li>
             <Link
               to="/admin/create"
